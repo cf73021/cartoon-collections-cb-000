05b0e8c244a5d10d1e7afcee7944559830e46eed
Chris Franklin <cf73021@gmail.com>
2018-07-20 15:44:27 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,7 @@
 def roll_call_dwarves(names)
   ordered_dwarves = []
-  names.each_with_index do |name|
+  names.each_with_index do |name, index|
+    name =
     ordered_dwarves << name
   end
   ordered_dwarves
