fc782f28ec92a04bda2f611b5b7da0c58f2dbb22
Chris Franklin <cf73021@gmail.com>
2018-07-20 15:49:32 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,12 +6,14 @@ def roll_call_dwarves(names)
     name = "#{index}" + "#{name}"
     ordered_dwarves << name
   end
-  
+
   puts ordered_dwarves
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.collect do |call|
+    call.upcase
+    
 end
 
 def long_planeteer_calls# code an argument here
