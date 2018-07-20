9029ea23e997d0f65605ee0040582a866405328c
Chris Franklin <cf73021@gmail.com>
2018-07-20 16:23:57 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,12 +19,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(calls)
-  calls.each do |call|
-    if call.length > 4
-      return true
-      else return false
-    end
-  end
+  calls.any? {|call| call.length > 4}
 end
 
 def find_the_cheese# code an argument here
